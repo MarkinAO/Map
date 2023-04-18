16158379d4374aef28574d3434965858062ae21e
MarkinAO <habar77@yandex.ru>
2023-04-18 22:23:27 +0700

    init commit

---
+++ src/__test__/test.js
@@ -0,0 +1,9 @@
+import ErrorRepository from '../js/main';
+
+const error = new ErrorRepository()
+
+test('Test method translate', () => {
+  expect(error.translate(400)).toBe('Bad Request');
+  expect(error.translate(404)).toBe('Not Found');
+  expect(error.translate(999)).toBe('Unknown error');
+});
